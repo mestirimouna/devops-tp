137cd9ec007347e7afb8ea89d907c29847e7e847
Mouna <mestirimouna2@gmail.com>
2025-02-24 17:32:12 +0100

    Add GitHub Actions workflow and hello-devops folder

---
+++ hello-devops/test_app.py
@@ -0,0 +1,10 @@
+# test_app.py
+import unittest
+
+# Simple test case
+class TestApp(unittest.TestCase):
+    def test_output(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
