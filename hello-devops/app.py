d2a7e6a8567a4ec36df2f9eb84bf60f651935992
Mouna <mestirimouna2@gmail.com>
2025-03-01 15:24:43 +0100

    Add /status route to check service status

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -1,1 +1,15 @@
-print ("Hello , CI/CD!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the homepage!"
+
+# Add the new route for /status
+@app.route('/status')
+def status():
+    return {"status": "ok", "message": "Service is running smoothly!"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
